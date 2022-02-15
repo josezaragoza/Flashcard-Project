9db809bc57c5aea6583a9e3d6152b8895510f4ef
Jose <zaragoza.jose@gmail.com>
2022-02-15 12:20:09 -0800

    Home

---
+++ src/Layout/Decks/DeckList.js
@@ -0,0 +1,3 @@
+export default function DeckList({decks}) {
+    return <h1>DeckList</h1>
+}
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { listDecks } from "../utils/api";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
 
 function Layout() {
+  const [decks, setDecks] = useState([]);
+  useEffect(() => {
+    const ac = new AbortController();
+    async function getDecks() {
+      try {
+        const receivedDecks = await listDecks(ac.signal);
+        setDecks(receivedDecks);
+      } catch (error) {
+        throw error;
+      }
+    }
+    getDecks();
+    return ac.abort();
+  }, []);
   return (
     <>
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route exact path="/">
+            <Home decks={decks} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </>
   );

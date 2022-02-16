ab8853008c4b6933821c95f427ed1ecf6caa4bb3
Jose <zaragoza.jose@gmail.com>
2022-02-16 10:37:27 -0800

    home

---
+++ src/Layout/Home.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import DeckList from "./Deck/DeckList";
+
+export default function Home({ decks }) {
+  return (
+    <div className="d-flex flex-column">
+      <div className="mb-2">
+        <Link className="btn btn-secondary" to="/decks/new">
+          <i className="fa-solid fa-plus"></i> Create Deck
+        </Link>
+      </div>
+
+      <div>
+        <DeckList decks={decks} />
+      </div>
+    </div>
+  );
+}

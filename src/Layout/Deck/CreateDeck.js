9b2bbefeb9d3b63ad72cc5698467ed80ddb4176d
Jose <zaragoza.jose@gmail.com>
2022-02-16 10:35:09 -0800

    cards and decks

---
+++ src/Layout/Deck/CreateDeck.js
@@ -0,0 +1,16 @@
+import NavBar from "../NavBar";
+import DeckForm from "./DeckForm";
+
+export default function CreateDeck() {
+  return (
+    <>
+      <div className="d-flex">
+        <NavBar pageName={"Create Deck"} />
+      </div>
+      <div className="d-flex flex-column">
+        <h2>Create Deck</h2>
+        <DeckForm mode="create" />
+      </div>
+    </>
+  );
+}
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -145,6 +145,20 @@ export async function deleteDeck(deckId, signal) {
 }
 
 /**
+ * Retrieves all cards associated with the specified `deckId`.
+ * @param deckId
+ *  the id of the target deck
+ * @param signal
+ *  optional AbortController.signal
+ * @returns {Promise<Error|*>}
+ *  a promise that resolves to a possible empty array of cards.
+ */
+export async function listCards(deckId, signal) {
+  const url = `${API_BASE_URL}/cards?deckId=${deckId}`;
+  return await fetchJson(url, { signal });
+}
+
+/**
  * Creates a new card associated with the specified `deckId`.
  * There is no validation that there is an existing deck with the specified `deckId`.
  * @param deckId

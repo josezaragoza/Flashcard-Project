c33c626a0d2d8c63e87b0debfdc7821aff69c7d7
Jose <zaragoza.jose@gmail.com>
2022-02-16 07:48:46 -0800

    decklist

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import DeckList from "./Decks/DeckList";
 
-export default function Home({decks}) {
-    return (
-        <div className='d-flex flex-column'>
-            <div className="mb-2 ">
-                <Link to="/decks/new" className="btn btn-secondary">Create Deck</Link>
-            </div>
-            <div>
-                <DeckList decks={decks}/>
-            </div>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="d-flex flex-column">
+      <div className="mb-2 ">
+        <Link to="/decks/new" className="btn btn-secondary">
+          Create Deck
+        </Link>
+      </div>
+      <div>
+        <DeckList />
+      </div>
+    </div>
+  );
 }
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import { listDecks } from "../utils/api";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 
 function Layout() {
-  const [decks, setDecks] = useState([]);
-  useEffect(() => {
-    const ac = new AbortController();
-    async function getDecks() {
-      try {
-        const receivedDecks = await listDecks(ac.signal);
-        setDecks(receivedDecks);
-      } catch (error) {
-        throw error;
-      }
-    }
-    getDecks();
-    return ac.abort();
-  }, []);
+
   return (
     <>
       <Header />
@@ -27,10 +12,10 @@ function Layout() {
         {/* TODO: Implement the screen starting here */}
         <Switch>
           <Route exact path="/">
-            <Home decks={decks} />
+            <Home />
           </Route>
           <Route>
-            <NotFound />
+        <NotFound />            
           </Route>
         </Switch>
       </div>
